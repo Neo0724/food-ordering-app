d1e83bcadb0738792cc04d6a92eefe57ebbc5621
JACKSON <hoekit041218@gmail.com>
2025-03-25 11:41:43 +0800

    feat: update findCartsByUserId query to filter by active cart status

--- server/food/src/main/java/com/example/canteen/food/repository/CartRepository.java
+++ server/food/src/main/java/com/example/canteen/food/repository/CartRepository.java
@@ -13,21 +13,21 @@ import java.util.List;
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
     @Query("SELECT new com.example.canteen.food.model.vo.CartVO( " +
-            "c.userId, " +
-            "c.cartId, " +
-            "c.itemId, " +
-            "v.size, " +
-            "i.itemName, " +
-            "c.sizeId, " +
-            "c.quantity, " +
-            "v.quantity, " +
-            "c.status, " +
-            "c.createTime, " +
-            "c.updateTime, " +
-            "v.price) " +
-            "FROM Cart c " +
-            "JOIN Item i ON c.itemId = i.itemId " +
-            "JOIN Variant v ON c.sizeId = v.sizeId " +
-            "WHERE c.userId = :userId")
-    List<CartVO> findCartsByUserId(@Param("userId") String userId);
+    "c.userId, " +
+    "c.cartId, " +
+    "c.itemId, " +
+    "v.size, " +
+    "i.itemName, " +
+    "c.sizeId, " +
+    "c.quantity, " +
+    "v.quantity, " +
+    "c.status, " +
+    "c.createTime, " +
+    "c.updateTime, " +
+    "v.price) " +
+    "FROM Cart c " +
+    "JOIN Item i ON c.itemId = i.itemId " +
+    "JOIN Variant v ON c.sizeId = v.sizeId " +
+    "WHERE c.userId = :userId AND c.status = com.example.canteen.food.model.dto.enums.CartStatus.ACTIVE")
+List<CartVO> findCartsByUserId(@Param("userId") String userId);
 }
